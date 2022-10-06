ad758f60f97e616abfab69620f7df5c2af069eab
Rasmus Djupedal <radj2000@student.miun.se>
2022-10-06 11:36:01 +0200

    Drawing functions are working

--- src/se/miun/dt176g/xxxxyyyy/reactive/MainFrame.java
+++ src/se/miun/dt176g/xxxxyyyy/reactive/MainFrame.java
@@ -1,44 +1,22 @@
 package se.miun.dt176g.xxxxyyyy.reactive;
 
-import java.awt.*;
 import javax.swing.*;
 
-/**
- * <h1>MainFrame</h1> 
- * JFrame to contain the rest
- *
- * @author 	Rasmus Djupedal
- * @version 1.0
- * @since 	2022-09-08
- */
-
-@SuppressWarnings("serial")
 public class MainFrame extends JFrame {
+    DrawModel dModel;
+    DrawView dView = new DrawView(this);
+    DrawController dController;
 
-	private String header;
-	private DrawingPanel drawingPanel;
+    public MainFrame() {
+        this.setSize(800, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("MS Paint sucks!");
 
-	public MainFrame() {
+        dModel = new DrawModel();
+        dController = new DrawController(dView, dModel);
 
-		// default window-size.
-		this.setSize(1200, 900);
-		// application closes when the "x" in the upper-right corner is clicked.
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(dView);
+    }
 
-		this.header = "Reactive Paint";
-		this.setTitle(header);
-
-		// Changes layout from default to BorderLayout
-		this.setLayout(new BorderLayout());
-
-		// Creates all necessary objects and adds them to the MainFrame (just one object right now)
-		drawingPanel = new DrawingPanel();
-		drawingPanel.setBounds(0, 0, getWidth(), getHeight());
-		this.getContentPane().add(drawingPanel, BorderLayout.CENTER);
-
-		this.setJMenuBar(new Menu(this));
-
-	}
-
-
+    public DrawController getController() { return dController; }
 }
--- src/se/miun/dt176g/xxxxyyyy/reactive/Point.java
+++ src/se/miun/dt176g/xxxxyyyy/reactive/Point.java
@@ -1,49 +1,17 @@
 package se.miun.dt176g.xxxxyyyy.reactive;
 
-
-
-/**
- * <h1>Point</h1> 
- *
- * @author 	Rasmus Djupedal
- * @version 1.0
- * @since 	2022-09-08
- */
-
 public class Point {
+    private int x, y;
 
-	private int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    public int getX() { return this.x; }
+    public int getY() { return this.y; }
 
-	public int x() {
-		return x;
-	}
-
-	public int y() {
-		return y;
-	}
-
-	public void x(int x) {
-		this.x = x;
-	}
-
-	public void y(int y) {
-		this.y = y;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		Point p = (Point) o;
-		return (x == p.x() && y == p.y());
-	}
-	
-	@Override
-	public String toString() {
-		return "["+x+","+y+"]";
-	}
-
+    public void print() {
+        System.out.println("X: " + this.x + " Y: " + this.y);
+    }
 }
